9a79ac90c80adf357c5eeecaabefbdb1f796bafe
ivandmitrievsky <ivan.dmitrievsky@gmail.com>
2014-03-28 15:16:33 +0400

    initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  armaoptima
+//
+//  Created by Ivan Dmitrievsky on 28/03/14.
+//  Copyright (c) 2014 Ivan Dmitrievsky. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+    return 0;
+}
+
