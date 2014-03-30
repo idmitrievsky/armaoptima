a05a4bef20f865d2a8932027e2e81ed1ec21f72b
ivandmitrievsky <ivan.dmitrievsky@gmail.com>
2014-03-30 15:05:14 +0400

    made use of scientific notation

--- main.cpp
+++ main.cpp
@@ -15,10 +15,10 @@ int main(int argc, const char * argv[])
     unsigned calls = 0;
     auto obj = [](double arg){ return std::sqrt(1 + arg * arg) + std::exp(-2*arg); };
     
-    LOG(gss(obj, 0, 1, 0.00000001, &calls));
+    LOG(gss(obj, 0, 1, 1e-3, &calls));
     LOG(calls);
     calls = 0;
-    LOG(fib(obj, 0, 1, 0.00000001, &calls));
+    LOG(fib(obj, 0, 1, 1e-3, &calls));
     LOG(calls);
     
     return 0;
