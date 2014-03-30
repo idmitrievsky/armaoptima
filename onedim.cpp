4214edbbc945e231187212e72e9f50b6236e0a45
ivandmitrievsky <ivan.dmitrievsky@gmail.com>
2014-03-30 15:04:39 +0400

    rewrote general shrink in a proper way

--- onedim.cpp
+++ onedim.cpp
@@ -29,24 +29,30 @@ double general_shrink(one_dim_fun obj, double segment_begin, double segment_end,
 {
     ASSERT(segment_end > segment_begin, "Segment length must be positive.");
     
+    double segment = segment_end - segment_begin;
+    double segment_prop_part = proportion * segment, step_from_bound = segment - segment_prop_part;
+    
     double left_bound  = segment_begin, right_bound = segment_end;
-    double left_probe  = left_bound + proportion * (right_bound - left_bound);
-    double right_probe = left_bound + right_bound - left_probe;
+    double left_probe  = left_bound + step_from_bound;
+    double right_probe = right_bound - step_from_bound;
     double left_mean   = obj(left_probe);
     double right_mean  = obj(right_probe);
     unsigned _calls = 2;
     
     for (;;)
     {
-        if (right_bound - left_bound < precision)
+        if (segment_prop_part < precision)
             break;
         
+        segment = segment_prop_part;
+        segment_prop_part = step_from_bound;
+        step_from_bound = segment - segment_prop_part;
         if (left_mean < right_mean)
         {
             right_bound = right_probe;
             right_probe = left_probe;
             right_mean  = left_mean;
-            left_probe  = left_bound + proportion * (right_bound - left_bound);
+            left_probe  = left_bound + step_from_bound;
             left_mean   = obj(left_probe);
         }
         else
@@ -54,25 +60,25 @@ double general_shrink(one_dim_fun obj, double segment_begin, double segment_end,
             left_bound  = left_probe;
             left_probe  = right_probe;
             left_mean   = right_mean;
-            right_probe = left_bound + right_bound - left_probe;
+            right_probe = right_bound - step_from_bound;
             right_mean  = obj(right_probe);
         }
         _calls += 1;
     }
     if (calls) *calls = _calls;
     
-    return left_probe;
+    return left_bound + segment / 2;
 }
 
 double gss(one_dim_fun obj, double segment_begin, double segment_end, double precision, unsigned *calls)
 {
-    double proportion = (3 - std::sqrt(5)) / 2;
+    double proportion = (std::sqrt(5) - 1) / 2;
     return general_shrink(obj, segment_begin, segment_end, precision, proportion, calls);
 }
 
 double fib(one_dim_fun obj, double segment_begin, double segment_end, double precision, unsigned *calls)
 {
-    double proportion = 1 - (double)fibs[0] / fibs[1];
     auto fibs = fib([=](unsigned F){ return (segment_end - segment_begin) / F <= precision; });
+    double proportion = (double)fibs[0] / fibs[1];
     return general_shrink(obj, segment_begin, segment_end, precision, proportion, calls);
 }
